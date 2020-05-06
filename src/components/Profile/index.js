e09e9b7f1fd03d6703259e28bc7de898e5d3d499
Felipe Suri <felipeheredia1234@gmail.com>
2020-05-06 15:06:49 -0300

    Added Profile

---
+++ src/components/Profile/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const Profile = () => (
+	<div className="Profile-wrapper">
+		<h1>Felipe Suri</h1>
+		<h2>Programador de 15 anos.</h2>
+	</div>
+)
+
+export default Profile
